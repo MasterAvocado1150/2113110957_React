11849b749d678d77f55e5f1b26d50fe3f7758034
MasterAvocado1150 <WorkSP.Git.MinthadA2507@gmail.com>
2024-07-13 11:12:13 +0700

    update react native 13/7/24

--- App.tsx
+++ App.tsx
@@ -1,11 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import AppFooter from "./components/AppFooter";
+import AppHeader from "./components/AppHeader";
 
-export default function App() {
+export default function App(): React.JSX.Element {
+
+  const onClickMe = () =>{
+    Alert.alert('hello Lab.js', 'gg');
+  }
+
+  const users = [
+    {id:1001, name:'Min'},
+    {id:1002, name:'Somsak'}
+  ]
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppHeader title="This is Header" year={2018}/>
+      <AppHeader title="This is Header"/>
+
+      <Text>Hello React Native</Text>
+      {
+        users.map((data,index)=>{
+          return(
+            <Text key={data.id}>
+              No. {index+1} ID: {data.id} Name: {data.name}
+            </Text>
+          )
+
+        })
+      }
+
+      <Button
+        title="Click Me"
+        onPress={onClickMe}
+        color='pink'
+        /* onPress={() => {
+          Alert.alert("Hi", "React Native is fun");
+        }} */
+      />
+      <AppFooter/>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
