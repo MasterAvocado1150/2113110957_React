2d027bf0ff4314cd9f31d275232eb1765788c7bc
MasterAvocado1150 <WorkSP.Git.MinthadA2507@gmail.com>
2024-08-10 10:06:28 +0700

    Alert Mail

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,22 +1,76 @@
-import { View, Text, TextInput, Button } from 'react-native'
-import React from 'react'
-import { stylesLogin } from '../styles/style'
+import { View, Text, TextInput, Button, Alert } from "react-native";
+import React, { useState } from "react";
+import { stylesLogin } from "../styles/style";
 
-const Login = () => {
+const Login = (): React.JSX.Element => {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [pass, setpass] = useState("");
+
+  const validateEmail = (email: string): boolean => {
+    const recheckEmail = /\S+@\S+\.\S+/;
+    return recheckEmail.test(email);
+  };
+
+  const handleSubmit = () => {
+    let erroeMessage = "";
+    if (!name) {
+      // Alert.alert("Error", "Please Enter Name", [{ text: "OK" }]); //if ถ้าไม่ได้พิมชื่อลงไปจะมีการแจ้งเตือน
+      // return;
+      erroeMessage += "Please Enter Name\n";
+    }
+
+    if (!email) {
+      // Alert.alert("Error", "Please Enter Email", [{ text: "OK" }]); //if ถ้าไม่ได้พิมชื่อเมลไปจะมีการแจ้งเตือน
+      // return;
+      erroeMessage += "Please Enter Email\n";
+    } else if (!validateEmail(email)) {
+      erroeMessage += "Invalid Email Format\n";
+    }
+
+    //ตรวจรหัสผ่าน
+    if (!pass) {
+      // Alert.alert("Error", "Please Enter Password", [{ text: "OK" }]); //if ถ้าไม่ได้พิมชื่อเมลไปจะมีการแจ้งเตือน
+      erroeMessage += "Please Enter Password\n";
+    } else if (pass.length < 6) {
+      erroeMessage += "Password must be at lease 6 characters\n";
+      // Alert.alert("Password must be at lease 6 characters");
+    }
+
+    if (erroeMessage) {
+      Alert.alert("Error", erroeMessage.trim(), [{ text: "OK" }]); //if ถ้าไม่ได้พิมชื่อและเมลไปจะมีการแจ้งเตือน
+      return;
+    }
+    Alert.alert("Alert","Success",[{text:"OK"}]);
+  };
+
   return (
-    <View style = {stylesLogin.container}>
+    <View style={stylesLogin.container}>
       <TextInput
-          style={stylesLogin.input}
-          placeholder="Enter Name"
-        />
-        <TextInput
-          style={stylesLogin.input}
-          placeholder="Enter Name"
-        />
+        style={stylesLogin.input}
+        placeholder="Enter Name"
+        value={name}
+        onChangeText={setname}
+      />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Name"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setemail}
+      />
 
-        <Button title='Submit'/>
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Password"
+        value={pass}
+        onChangeText={setpass}
+        secureTextEntry={true}
+      />
+
+      <Button title="Submit" onPress={handleSubmit} />
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
